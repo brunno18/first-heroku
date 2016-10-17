3008cc0e395105703e963f6dd3f85500a778a762
Brunno Moreira <brunnomoreira.silva@gmail.com>
2016-10-17 15:43:23 -0200

    add say controller

---
+++ app/controllers/SayController.php
@@ -0,0 +1,17 @@
+<?php
+
+class SayController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+    
+    public function helloAction()
+    {
+        echo "Hello!";
+    }
+
+}
+
